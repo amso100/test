c1ce0d4e4375af0f25ee4812187d57b9ce83d0b9
Amit <amit.so100@gmail.com>
2016-03-18 14:17:51 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("Please enter a number: ");
+    scanf("%d", &x);
+    printf("The number you've entered is: %d", x);
+    return 0;
+}
