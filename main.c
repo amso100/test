355150e3e4a3619a52abf1765e356b0532eb5ec1
Amit <amit.so100@gmail.com>
2016-03-18 14:42:55 +0200

    now working?

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main()
 {
     int x;
-    printf("Please enter a number: ");
+    printf("Please enter a number that you want to enter: ");
     scanf("%d", &x);
     printf("The number you've entered is: %d", x);
     return 0;
